1feba73fa399a973931b877d42041cfbd7702bdc
fateme <fateme.ahmadi.asouri.1983@gmail.com>
2024-04-29 09:58:15 +0330

    footer complete

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Nav } from "../component/header/Nav";
+import { Footer } from "../component/Footer/Footer";
 
 const LayOut = () => {
     return ( 
         <>
            <Nav />
             <Outlet/>
-            Footer
+            <Footer/>
         </>
      );
 }
