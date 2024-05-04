b4cee08b10b6929478aa349dd64c03547c8c66b7
fateme <fateme.ahmadi.asouri.1983@gmail.com>
2024-05-04 19:34:49 +0330

    forget step1 complete

--- src/component/LoginComponent/License.jsx
+++ src/component/LoginComponent/License.jsx
@@ -1,6 +1,6 @@
 const License = () => {
     return ( 
-        <div className="text-white bg-violec h-6 w-full text-center">
+        <div className="text-white bg-violec h-12 py-3 w-full text-center">
             تمامی حقوق این سایت متعلق به تیم مارفا می باشد.
         </div>
      );
