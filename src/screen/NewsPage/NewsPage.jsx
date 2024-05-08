d51672a77c7567ab13b557c78143bf7a601c9def
Arezooshah <arezooshahabi@gmail.com>
2024-05-08 16:37:30 +0330

    END Edit NewsMainPage

--- src/screen/NewsPage/NewsPage.jsx
+++ src/screen/NewsPage/NewsPage.jsx
@@ -1,13 +1,35 @@
-import React from "react";
-import NewsDitailsPage from "./NewsDetailsPage";
+import React from 'react'
+
+import ItemFilterNews from '../../component/NewsDetailsComponent/ItemFilterNews'
+import Topheaderseconde from '../../component/CourseComponent/Topheaderseconde'
+import DataNews from '../../component/NewsDetailsComponent/DataNews.json'
+import CardNews from '../../component/LandingComponent/News/CardNews'
 
 
-const NewsPage = () => {
-    return ( 
-        <div>
-            <NewsDitailsPage/>
+const NewsMaineScreen = () => {
+  
+  return (
+    <div  className="bg-bluec w-full">
+       <div className="bg-[#D7D5FF] shadow-2xl shadow-bg-bluec mx-auto max-w-[1920px]">
+          <Topheaderseconde/>
+          <ItemFilterNews/>
+          <div className='flex flex-col justify-center items-center mx-auto px-5 w-[90%]' >
+            {DataNews.map((item) => {
+            return(
+            <div  className='w-[90%]' key={item.id}> 
+              <CardNews 
+              title={item.title}
+              img={item.img}
+              view={item.view}
+              date={item.date}
+              description={item.description}
+              />
+            </div>
+            )
+          })}
         </div>
-     );
+      </div>
+    </div>
+  )
 }
- 
-export default NewsPage;
+export default NewsMaineScreen
