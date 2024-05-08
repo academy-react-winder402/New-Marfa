28442643da64602b81bd84dc4f9bea70fc13181c
Arezooshah <arezooshahabi@gmail.com>
2024-05-08 16:53:42 +0330

    End Edit CardNews Page Landing

--- src/component/LandingComponent/News/CardNews.jsx
+++ src/component/LandingComponent/News/CardNews.jsx
@@ -1,30 +1,40 @@
 import React from 'react'
 
-
-const CardNews = ({image}) => {
+import { IoEyeOutline } from "react-icons/io5";
+import { CiCalendarDate } from "react-icons/ci";
+// import image from "../../assets/image/N1.png"
+const CardNews = ({title , img, view , date, description}) => {
   return (
-   <>
-     
-     <div className="m-5 mb-10 flex flex-col items-center justify-center rounded-lg border border-gray-200 bg-white shadow hover:bg-gray-100 md:flex-row dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-         <div className="my-5 h-96 w-full rounded-t-lg px-8 md:h-auto md:rounded-none md:rounded-s-lg">
-          <img className="mx-auto w-[80%] object-cover md:w-[100%]" src={image} alt="" />
+       
+     <div className="flex md:flex-row flex-col justify-center items-center gap-5 border-gray-200 dark:border-gray-700 bg-white hover:bg-gray-100 dark:hover:bg-gray-700 dark:bg-gray-800 shadow mx-auto my-10 border rounded-lg w-full">
+         <div className="flex justify-center items-center m-5 rounded-t-lg md:rounded-none md:rounded-s-lg md:w-[40%]">
+          <img className="rounded-lg h-52 object-cover" src={img} alt="" />
          </div>
-         <div className='leading-8'>
+         <div className='flex flex-col flex-grow my-5 w-[80%] md:w-[50%] text-lg text-start leading-8'>
             <div className="flex flex-col justify-between p-4 leading-normal">
-                <h5 className="mb-2 text-center text-2xl font-bold tracking-tight text-indigo-900 md:text-[15px] dark:text-white"> چگونه برنامه نویسی را آسان تر یاد بگیریم؟</h5>
-                <p className="m-8 mt-10 text-start font-normal text-gray-700 dark:text-gray-400">دوره های آنلاین انعطاف  پدیری بیشتری را در برنامه ریزی روز خود و کار در کنار خانواده امکان پدیر می سازد.</p>
+                <h5 className="mb-2 font-bold text-2xl text-indigo-900 md:text-[15px] dark:text-white tracking-tight">  {title}</h5>
+                <div className='flex flex-row gap-3 text-sm text-zinc-500'>
+                <div className='flex flex-row justify-center items-center'> 
+                    <IoEyeOutline />
+                     <span> {view}  بازدید  |</span>
+                  </div>
+                  <div className='flex flex-row justify-center items-center'> 
+                  <CiCalendarDate />
+                     <span>{date}</span>
+                  </div>
+                </div>
+                <p className="mt-10 font-normal text-gray-700 dark:text-gray-400">{description}</p>
             </div>
-            <div className='mx-5 mb-10 text-end'>
-            <a href="#" className="ml-20 rounded-lg py-3 align-bottom text-sm font-medium text-indigo-900 hover:bg-indigo-700 focus:outline-none focus:ring-4 focus:ring-blue-300 md:items-center md:text-start dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              ادامه مطلب
-              <svg className="absolute left-32 -mt-3 h-3 w-3 rotate-180 transform text-left md:left-40" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-              </svg>
-            </a>
-            </div>
+            <div className='flex justify-end items-end mb-10'>
+           <a href="#" className="relative align-bottom md:items-center hover:bg-indigo-700 dark:hover:bg-blue-700 dark:bg-blue-600 ml-2 p-3 rounded-lg w-36 font-medium text-center text-indigo-900 text-sm focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800">
+             ادامه مطلب
+             <svg className="left-5 absolute flex -mt-3 w-3 h-2 transform rotate-180"  aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+             </svg>
+           </a>
+           </div>
          </div>
       </div> 
-   </>
   ) 
   }  
 
--- src/component/LandingComponent/News/News.jsx
+++ src/component/LandingComponent/News/News.jsx
@@ -1,25 +1,37 @@
 import CardNews from "./CardNews";
-import card1 from "../../../assets/image/N2.png"
-import card2 from "../../../assets/image/N1.png"
+import DataNews from "../../NewsDetailsComponent/DataNews.json";
+import TitrComponent from "../../../common/TitrComponent/TitrComponent";
+import AllButton from "../../../common/button/AllButton";
+
+
 
 const News = () => {
-    return(
-        <>
-            <div>
-                <div className="mb-5 mt-10 flex w-full flex-col items-center text-end text-lg">
-                        <span className="block w-36 text-2xl text-indigo-900">اخبار و مقالات </span>
-                        <span className="block w-36 bg-gradient-to-l from-violet-900 to-transparent py-[2px]"></span>
-                </div>
-                <div className="flex w-[95%] flex-col lg:flex-row lg:justify-center">
-                    <CardNews image={card2} />
-                    <CardNews  image={card1}/>
-                </div>
-                <div className="relative h-32 w-full">
-                    <button type="submit" className="absolute inset-0 right-40 mb-20 h-16 w-36 rounded-lg bg-blue-700 px-4 py-1 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 md:right-[35rem] dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> نمایش بیشنر</button>
-                </div>
+  return (
+      <div className="w-full">
+        <TitrComponent title=" اخبار و مقالات "/>
+        
+        <div className="flex md:flex-row flex-col flex-wrap justify-center items-center gap-5 mx-auto w-full md:w-[95%]">
+         
+          {DataNews.map((item) => {
+            return(
+            <div  className='w-[95%] md:w-[45%]'  key={item.id}> 
+              <CardNews 
+              title={item.title}
+              img={item.img}
+              view={item.view}
+              date={item.date}
+              description={item.description}
+              />
             </div>
-        </>
-    )
-}
+            )
+          })}
+         
+        </div>
+       
+        <AllButton title="نمایش بیشتر " />
+      </div>
+
+  );
+};
 
 export default News;
